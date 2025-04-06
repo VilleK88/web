6c38f930dd74034a82de01ea06cd9174aa423c8a
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-04-06 10:57:33 +0300

    3. tehtävä 3. tehty.

--- t3/3.js
+++ t3/3.js
@@ -1,2 +1,14 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+displayList(names);
+
+function displayList(names) {
+    let ul = document.createElement("ul");
+    for(let i = 0; i < names.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = names[i];
+        ul.appendChild(li);
+    }
+    document.getElementById("target").appendChild(ul);
+}
