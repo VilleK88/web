8e9b724df1ff62d353d0b9d86e340bcc981ac001
VK88 <126750132+VilleK88@users.noreply.github.com>
2025-04-06 10:44:56 +0300

    3. tehtävä 1. tehty.

---
+++ t1/1.js
@@ -0,0 +1,7 @@
+displayList();
+
+function displayList() {
+    const target = document.getElementById("target");
+    target.innerHTML = "<li>First item</> <li>Second item</> <li>Third item</>";
+    target.classList.add("my-list");
+}
